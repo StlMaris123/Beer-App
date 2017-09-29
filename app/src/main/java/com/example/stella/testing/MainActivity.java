68390e2336b45a2f4a72cc4badc3702daff128b5
StlMaris123 <stellamarisnjage@gmail.com>
2017-09-29 16:39:40 +0300

    call when button gets clicked

--- app/src/main/java/com/example/stella/testing/MainActivity.java
+++ app/src/main/java/com/example/stella/testing/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.stella.testing;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    //call when the button gets clicked
+    public void onClickFindBeer(View view){
+        //get reference to the text view
+        TextView brands = (TextView) findViewById(R.id.brands);
+        //get reference to the spinner
+        Spinner color = (Spinner) findViewById(R.id.color);
+        //get the selected item in the spinner
+        String beerType = String.valueOf(color.getSelected());
+        //display the selected types
+        brands.setText(beerType);
+
     }
 }
